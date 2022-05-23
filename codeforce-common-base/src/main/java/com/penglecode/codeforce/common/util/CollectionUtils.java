6c0059290ec37f731fed865925fd6d1e18642a52
penggle <pengpeng.prox@gmail.com>
2022-05-24 00:02:58 +0800

    commit

--- codeforce-common-base/src/main/java/com/penglecode/codeforce/common/util/CollectionUtils.java
+++ codeforce-common-base/src/main/java/com/penglecode/codeforce/common/util/CollectionUtils.java
@@ -1,6 +1,9 @@
 package com.penglecode.codeforce.common.util;
 
+import org.springframework.util.Assert;
+
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author pengpeng
@@ -68,6 +71,50 @@ public class CollectionUtils extends org.springframework.util.CollectionUtils {
                 return iterator.next();
             }
         };
+    }
+
+    /**
+     * 求笛卡尔积
+     *
+     * @param groupedLists
+     * @param <T>
+     * @return
+     */
+    public static <T> List<List<T>> cartesianProduct(List<List<T>> groupedLists) {
+        if(isEmpty(groupedLists)) {
+            return Collections.emptyList();
+        }
+        Assert.noNullElements(groupedLists, "The element of 'groupedLists' must be not empty!");
+        //利用LinkedList的poll()特性
+        LinkedList<List<T>> groupedElements = new LinkedList<>(groupedLists);
+        List<List<T>> startElements = new ArrayList<>();
+        startElements.add(groupedElements.poll());
+        List<List<T>> resultElements = new ArrayList<>();
+        recursiveCartesianProduct(startElements, groupedElements, resultElements);
+        return resultElements;
+    }
+
+    private static <T> void recursiveCartesianProduct(List<List<T>> startElements, LinkedList<List<T>> remainingElements, List<List<T>> resultElements) {
+        if(!isEmpty(remainingElements)) {
+            List<T> remainingFirst = remainingElements.poll();
+            List<List<T>> newStartElements = startElements.stream().flatMap(startElement -> remainingFirst.stream().map(element -> {
+                List<T> subCartesian = new ArrayList<>(startElement);
+                subCartesian.add(element);
+                return subCartesian;
+            })).collect(Collectors.toList());
+            recursiveCartesianProduct(newStartElements, remainingElements, resultElements);
+        } else {
+            resultElements.addAll(startElements);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> group1 = Arrays.asList("1", "2", "3");
+        List<String> group2 = Arrays.asList("红", "绿", "蓝");
+        List<String> group3 = Arrays.asList("A", "B", "C");
+        List<List<String>> groupedLists = Arrays.asList(group1, group2, group3);
+        groupedLists = cartesianProduct(groupedLists);
+        groupedLists.forEach(System.out::println);
     }
 
 }
