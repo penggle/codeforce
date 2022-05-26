9ed4002de3f94e0020d6b385db89cad91ed3407d
pengpengc <pengpengc@enn.cn>
2022-05-26 12:18:53 +0800

    commit

---
+++ codeforce-common-config/src/main/java/com/penglecode/codeforce/common/config/defaults/CommonDefaultConfigBasePackage.java
@@ -0,0 +1,10 @@
+package com.penglecode.codeforce.common.config.defaults;
+
+/**
+ * 公共的默认配置BasePackage
+ *
+ * @author pengpeng
+ * @version 1.0
+ */
+public class CommonDefaultConfigBasePackage {
+}
