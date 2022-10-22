09f5a68e91494bb64392783a18aaeff632523996
penggle <pengpeng.prox@gmail.com>
2022-10-22 17:23:52 +0800

    commit

--- codeforce-common-model/src/main/java/com/penglecode/codeforce/common/model/BaseRequest.java
+++ codeforce-common-model/src/main/java/com/penglecode/codeforce/common/model/BaseRequest.java
@@ -1,5 +1,7 @@
 package com.penglecode.codeforce.common.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * 基础请求
  *
@@ -8,7 +10,11 @@ package com.penglecode.codeforce.common.model;
  */
 public abstract class BaseRequest implements BaseDTO {
 
+    @Schema(description="traceId(唯一,一般为UUID)")
     private String traceId;
+
+    @Schema(description="是否级联操作(某些业务场景下需要)", defaultValue="false")
+    private boolean cascade;
 
     public String getTraceId() {
         return traceId;
@@ -16,6 +22,14 @@ public abstract class BaseRequest implements BaseDTO {
 
     public void setTraceId(String traceId) {
         this.traceId = traceId;
+    }
+
+    public boolean isCascade() {
+        return cascade;
+    }
+
+    public void setCascade(boolean cascade) {
+        this.cascade = cascade;
     }
 
 }
